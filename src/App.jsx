14650adebe1c5741b4256403ab7ddb74f7310033
DinhVien <vodinhvien0601@gmail.com>
2020-10-24 08:50:36 +0700

    Tao thu vien va cau truc cơ ban

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World <code>src/App.js</code>Welcom 
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> 
       </header>
     </div>
   );
